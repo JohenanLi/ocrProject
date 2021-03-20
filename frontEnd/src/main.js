289a37a4f964b27c26586a2115b6b8c133a08528
JohenanLi <599192367@qq.com>
2021-03-20 12:27:17 +0800

    okok

--- frontEnd/src/main.js
+++ frontEnd/src/main.js
@@ -4,12 +4,21 @@ import router from './router'
 import store from './store/index.js'
 import './assets/lib/flexible.js'
 import './assets/lib/Chart'
+
 /**使用vux的插件提示功能*/
 import  { ToastPlugin } from 'vux'
+import { Tabbar, TabbarItem, NavBar, Tab, Tabs, Grid, GridItem, Divider, DropdownMenu, Toast,
+    DropdownItem, Switch, Button, List, Cell, CellGroup, PullRefresh, Row, Col, Field, ActionSheet, Icon, Image} from "vant"
+import 'vant/lib/index.css'
+import http from '@/utils/http'
 Vue.use(ToastPlugin)
-
+Vue.use(Tabbar).use(TabbarItem).use(NavBar).use(Tab).use(Tabs).use(Grid).use(GridItem).use(Divider).use(DropdownMenu)
+    .use(DropdownItem).use(Switch).use(Button).use(List).use(Cell).use(CellGroup).use(PullRefresh).use(Row).use(Col)
+    .use(Field).use(ActionSheet).use(Icon).use(Image).use(Toast);
+Vue.use(axios);
 Vue.config.productionTip = false
 
+Vue.prototype.$http = http;
 new Vue({
     el: '#app',
     router,
---
+++ frontEnd/src/views/user/User.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="container-view">
+    <nav-bar></nav-bar>
+    <section
+      style="padding: 30px 30px; display: flex; flex-flow: column nowrap"
+    >
+      <div style="font-size: 20px; font-weight: bold">手机号密码登录</div>
+      <div>
+        <van-cell-group>
+          <van-field
+            v-model="tel"
+            clearable
+            placeholder="请输入手机号"
+            style="background-color: WhiteSmoke"
+            ref="phone"
+          >
+          </van-field>
+          <van-field
+            v-model="pwd"
+            center
+            clearable
+            placeholder="请输入密码"
+            style="background-color: WhiteSmoke"
+            ref="pwd"
+            type="password"
+          >
+          </van-field>
+        </van-cell-group>
+        <span style="color: red" v-show="errMsg">{{ errMsg }}</span>
+      </div>
+      <div style="background-color: DarkGray">
+        <van-button :type="type" block :disabled="loginDisabled" @click="login"
+          >登录</van-button
+        >
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+// 引入组件Cell CellGroup
+import { Cell, CellGroup, Icon } from "vant";
+import NavBar from "@/components/NavBar";
+import { loginByPwd } from "@/api";
+export default {
+  name: "Login",
+  mounted() {
+    // 输入框聚焦
+    if (!this.tel) {
+      this.$refs.phone.focus();
+    } else {
+      this.$refs.pwd.focus();
+    }
+  },
+  watch: {
+    // 监听手机号和密码输入框，修改登录按钮状态和类型
+    pwd(newVal) {
+      if (newVal.length > 0) {
+        this.loginDisabled = false;
+        this.type = "danger";
+      } else {
+        this.loginDisabled = "disabled";
+        this.type = "default";
+      }
+    },
+    tel(newVal, oldVal) {
+      this.tel = this.tel.replace(/\s+/g, "").replace(/\D/g, "");
+      if (isNaN(newVal) || newVal.length > 11) {
+        this.tel = oldVal;
+        return;
+      }
+      if (this.pwd.length > 0) {
+        this.loginDisabled = false;
+        this.type = "danger";
+      } else {
+        this.loginDisabled = "disabled";
+        this.type = "default";
+      }
+    },
+  },
+  data() {
+    return {
+      // 登录按钮类型
+      type: "default",
+      // 手机号
+      tel: this.$store.state.phone,
+      // 密码
+      pwd: "",
+      // 错误提示信息
+      errMsg: "",
+      // 登录按钮是否禁用
+      loginDisabled: "disabled",
+    };
+  },
+  methods: {
+    // 登录
+    loginByPwd(params) {
+      axios.get();
+    },
+    login() {
+      let params = {
+        username: this.tel,
+        password: this.pwd,
+      };
+      loginByPwd(params).then((res) => {
+        if (res.code !== 200) {
+          this.errMsg = res.msg;
+          // 密码输入错误时重新聚焦输入框
+          this.$refs.pwd.focus();
+        } else {
+          this.$store.commit("login", res.data);
+        }
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+section div {
+  margin-bottom: 16px;
+}
+</style>
+</template>
